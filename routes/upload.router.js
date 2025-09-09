081f10e35bcb7ab204a07c569657a054e2a0cccd
Ivan Vitkovskiy <cloudfly97@icloud.com>
2025-09-09 22:17:07 +0300

    solve some issue

--- routes/upload.router.js
+++ routes/upload.router.js
@@ -3,7 +3,7 @@ const router = Router();
 const express = require("express");
 const app = express();
 const user = require("../models/user");
-app.use(express.json());
+
 const fileMiddleware = require("../middleware/fileMiddleware");
 const { requireAuth } = require("../middleware/authMiddleware");
 
@@ -16,10 +16,10 @@ router.post(
       const userID = req.user.id;
       const avatarUrl = `/avatars/${req.file.filename}`;
       await user.findByIdAndUpdate(userID, { avatar: avatarUrl });
-      res.json({ success: true, avatarUrl });
+      res.send({ avatarUrl });
     } catch (e) {
       res.status(500).json({ message: "Upload avatar error" });
     }
   }
 );
-module.exports = router;
+module.exports = router ;
